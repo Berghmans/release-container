444c983f66d8bce51292707a69b1feb287f5d29d
Olivier Berghmans <olivier@cloudar.be>
2024-05-30 14:54:39 +0200

    Test without containers

--- tests/sam-cli/tasks.py
+++ tests/sam-cli/tasks.py
@@ -7,7 +7,17 @@ from pathlib import Path
 @task
 def build(ctx):
     """Build and package the assets"""
-    ctx.run("sam build --config-file samconfig.toml")
+    ctx.run("sam build --base-dir . --build-dir out --template template.yaml --no-beta-features --use-container")
+    assert Path("out").exists()
+    assert Path("out/template.yaml").exists()
+    assert Path("out/exampleLambda").exists()
+    assert Path("out/exampleLambda/boto3").exists()
+
+
+@task
+def build_without_dind(ctx):
+    """Build and package the assets"""
+    ctx.run("sam build --base-dir . --build-dir out --template template.yaml --no-beta-features")
     assert Path("out").exists()
     assert Path("out/template.yaml").exists()
     assert Path("out/exampleLambda").exists()
