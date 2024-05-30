03cf7b1bead23ef97ab471fa2051fcf9d54bd233
Olivier Berghmans <olivier@cloudar.be>
2024-05-30 18:34:44 +0200

    Adjust the python version in the sam cli test

--- tests/sam-cli/tasks.py
+++ tests/sam-cli/tasks.py
@@ -1,12 +1,26 @@
 """Invoke tasks file"""
 
+import re
 from invoke import task
 from pathlib import Path
 
 
+def update_runtime_in_template(file_path: Path, new_runtime: str):
+    with file_path.open(mode="r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Use regex to replace the Runtime line
+    updated_content = re.sub(r"Runtime:\s*python[0-9\.]+", f"Runtime: {new_runtime}", content)
+
+    with file_path.open(mode="w", encoding="utf-8") as file:
+        file.write(updated_content)
+
+
 @task
-def build(ctx):
+def build(ctx, runtime):
     """Build and package the assets"""
+    template_path = Path("template.yaml")
+    update_runtime_in_template(template_path, f"python{runtime}")
     ctx.run("sam build --base-dir . --build-dir out --template template.yaml --no-beta-features --use-container")
     assert Path("out").exists()
     assert Path("out/template.yaml").exists()
@@ -17,8 +31,10 @@ def build(ctx):
 
 
 @task
-def build_without_dind(ctx):
+def build_without_dind(ctx, runtime):
     """Build and package the assets"""
+    template_path = Path("template.yaml")
+    update_runtime_in_template(template_path, f"python{runtime}")
     ctx.run("sam build --base-dir . --build-dir out --template template.yaml --no-beta-features")
     assert Path("out").exists()
     assert Path("out/template.yaml").exists()
